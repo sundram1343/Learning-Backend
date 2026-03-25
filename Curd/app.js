078763a6390a0924f824ef27433a59960dc72a5d
Sundram1343 <sundramsharma2901@gmail.com>
2026-03-25 16:59:08 +0530

    learning db

--- Curd/app.js
+++ Curd/app.js
@@ -1,6 +1,23 @@
 const express=require('express');
 const app=express();
+const userModel=require('./usermodel')
 app.get('/',(req,res)=>{
     res.send("Hey");
+})
+app.get('/create',async (req,res)=>{
+    let createuser= await userModel.create({
+        name:'harsh',
+        email:'harsh@gmail.com',
+        username:'harsh'
+    })
+    res.send(createuser);
+})
+app.get('/update',async (req,res)=>{
+    let updateduser=await userModel.findOneAndUpdate({username:'harsh'},{name:'Sundram'},{returnDocument:'after'});
+    res.send(updateduser);
+})
+app.get('/read',async (req,res)=>{
+    let users=await userModel.find();
+    res.send(users);
 })
 app.listen(3000)
